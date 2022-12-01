3bf4ae8d930a5811bce8f0c4ad4a6de2fc47595f
bire5 <lifayamga@gmail.com>
2022-11-30 16:53:51 -0800

    Check if uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+
 def uppercase(str):
-    for x in str:
-        if ord(x) >= 97 and ord(x) <= 122:
-            c = chr(ord(x) - 32)
-            print("{}".format(x), end="")
-    print("")
+    for i in range(len(str)):
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            upp = ord(str[i]) + (ord('A') - ord('a'))
+        else:
+            upp = ord(str[i])
+            print("{}".format(chr(upp)), end='')
+    print()
