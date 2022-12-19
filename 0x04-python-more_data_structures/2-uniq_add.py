86b39c74cccfa873fae006f5b390521635b084e2
bire5 <lifayamga@gmail.com>
2022-12-18 18:31:53 -0800

    Add unique integers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+
+    sum = 0
+    new_list = sorted(my_list)
+    for i in range(len(new_list)):
+        if i == 0 or new_list[i] != new_list[i - 1]:
+            sum += new_list[i]
+    return(sum)
