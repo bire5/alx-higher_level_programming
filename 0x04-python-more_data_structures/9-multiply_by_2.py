dff24fc9f8584e930d2bb13bbc71503d7bdae88a
bire5 <lifayamga@gmail.com>
2022-12-18 20:01:25 -0800

    Delete a key in the victory

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+    new_dict = {}
+    for k, v in a_dictionary.items():
+        new_dict[k] = v * 2
+    return(new_dictionary)
