b2ba1c68b88b68ec9366296d74ef577841434df3
bire5 <lifayamga@gmail.com>
2022-12-21 22:04:50 -0800

    Print square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -21,7 +21,7 @@ and public instance method."""
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-       self.__size = value
+        self.__size = value
 
     def area(self):
         """calculates and returns current square area"""
