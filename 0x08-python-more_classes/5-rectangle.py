e8b8c2e3707f1df69710d2259a4a4e53c7d9c557
bire5 <lifayamga@gmail.com>
2023-01-03 21:26:04 -0800

    Detect instance deltion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            width(int): width of the new rectangle.
+            height(int): height of the new rectangle.
         """
         self.width = width
         self.height = height
@@ -52,7 +52,7 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
-        Represents the rectangle with the # character.
+        Represents the rectangle with the  # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
@@ -71,5 +71,5 @@ class Rectangle:
         return (rect)
 
     def __del__(self):
-        """Print a message for every deletion of a Rectangle."""
+        """Print a message for every deletion of a Rectangle"""
         print("Bye rectangle...")
